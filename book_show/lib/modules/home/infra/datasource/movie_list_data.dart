1188ff2aead82c9bb780f95787a60242835918b6
vikramkadam <vikram.kadam@thoughtworks.com>
2023-08-16 17:38:06 +0530

    Added api implementation for success, error and loading.

--- book_show/lib/modules/home/infra/datasource/movie_list_data.dart
+++ book_show/lib/modules/home/infra/datasource/movie_list_data.dart
@@ -1,16 +1,30 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:book_show/core/config/app_config.dart';
+import 'package:book_show/modules/home/infra/models/movie.dart';
 
 class MovieDataSource {
-  Future<http.Response> getNowShowingMovieList() async {
+  Future<List<Movie>> getNowShowingMovieList() async {
     Map<String, String> headers = {
       "Content-Type": "application/json",
       "Accept": "application/json",
       "Authorization": "Bearer ${Config.API_KEY}"
     };
-    http.Response response = await http.get(Uri.parse(
-        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"), headers: headers);
-    print(response.body);
-    return response;
+    http.Response response = await http.get(
+        Uri.parse(
+            "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1"),
+        headers: headers);
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var results = data["results"] as List;
+      List<Movie> movies = [];
+      results.forEach((element) {
+        movies.add(Movie.fromJson(element));
+      });
+      return movies;
+    } else {
+      throw Exception("Something went Wrong..!!!");
+    }
   }
 }
--- book_show/lib/modules/home/infra/models/movie.dart
+++ book_show/lib/modules/home/infra/models/movie.dart
@@ -1,3 +1,38 @@
-class Movie {
+import 'package:json_annotation/json_annotation.dart';
 
+part 'movie.g.dart';
+
+@JsonSerializable()
+class Movie {
+  double id = -1;
+  @JsonKey(defaultValue: false)
+  final bool adult;
+  @JsonKey(name: 'original_language', defaultValue: "")
+  final String originalLanguage;
+  @JsonKey(name: 'original_title', defaultValue: "")
+  String originalTitle = "";
+  @JsonKey(defaultValue: "")
+  String overview;
+  @JsonKey(defaultValue: 0.0)
+  double popularity;
+  @JsonKey(name: 'poster_path')
+  String posterPath = "";
+  @JsonKey(name: 'release_date')
+  String releaseDate = "";
+  String title = "";
+
+  Movie(
+      this.id,
+      this.adult,
+      this.originalLanguage,
+      this.originalTitle,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.releaseDate,
+      this.title);
+
+  factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MovieToJson(this);
 }
