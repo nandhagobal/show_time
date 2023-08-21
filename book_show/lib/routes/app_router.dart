858d4e52141b4d929e4d58174289769fbdbd0a43
vikramkadam <vikram.kadam@thoughtworks.com>
2023-08-21 13:53:56 +0530

    - Added launcher screen with flutter native splash.

--- book_show/lib/routes/app_router.dart
+++ book_show/lib/routes/app_router.dart
@@ -3,18 +3,10 @@ import 'package:book_show/routes/route_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../modules/onboarding/ui/launcher_screen.dart';
-
 class AppRouter {
   final goRouter = GoRouter(routes: [
     GoRoute(
         path: "/",
-        name: AppRouteConstants.launcherScreen,
-        pageBuilder: (context, state) {
-          return const MaterialPage(child: LauncherScreen());
-        }),
-    GoRoute(
-        path: "/home",
         name: AppRouteConstants.homeScreen,
         pageBuilder: (context, state) {
           return const MaterialPage(child: HomeScreen());
